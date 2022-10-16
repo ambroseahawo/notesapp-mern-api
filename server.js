832dbdb73c367281753fdcc5fdf8fb2e4e4dd990
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-10-16 13:09:55 +0300

    Implement cors options

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express")
 const path = require("path")
 const { logger } = require("./middleware/logger")
 const errorHandler = require("./middleware/errorHandler")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const corsOptions = require("./config/corsOptions")
 
 const app = express()
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
+app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
+
 app.use("/", express.static(path.join(__dirname, "public")))
 app.use('/', require("./routes/root"))
 
