0d387619082e501208f97ff4c1f71c9a294e3a00
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-12-03 15:40:54 +0300

    Users controllers - Get and Post

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,25 +6,62 @@ const bcrypt = require("bcrypt")
 // @desc Get all Users
 // @route GET /users 
 // @access Private
-const getAllUsers = asyncHandler(async(req, res) =>{})
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find().select("-password").lean()
+  if (!users) {
+    // return bad request response
+    return res.status(400).json({ message: "No users found." })
+  }
+  res.json(users)
+})
 
 
 // @desc Create new User
 // @route POST /users 
 // @access Private
-const createNewUser = asyncHandler(async(req, res) =>{}) 
+const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body
+
+  // confirm data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    // return bad request response
+    return res.status(400).json({ message: "All fields are required" })
+  }
+
+  // check for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec()
+
+  if (duplicate) {
+    // return conflict response
+    return res.status(409).json({ message: "User Already Exists." })
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10) // salt rounds
+
+  const newUUserObj = { username, "password": hashedPassword, roles }
+
+  // create and store new user
+  const user = await User.create(newUUserObj)
+
+  if (user) {
+    res.status(201).json({ message: `New user ${username} created` })
+  } else {
+    res.status(400).json({ message: "Invalid user data received" })
+  }
+})
 
 
 // @desc Update a User
 // @route PATCH /users 
 // @access Private
-const updateUser = asyncHandler(async(req, res) =>{})
+const updateUser = asyncHandler(async (req, res) => { })
 
 
 // @desc Delete a User
 // @route DELETE /users 
 // @access Private
-const deleteUser = asyncHandler(async(req, res) =>{})
+const deleteUser = asyncHandler(async (req, res) => { })
 
 module.exports = {
   getAllUsers,
