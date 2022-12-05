bf6ba397fee55b61f97c3deedca682a16998683f
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-12-05 14:57:41 +0300

    Implement Note Routes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,2 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const notesControllers = require("../controllers/notesControllers")
+
+router.route("/")
+  .get(notesControllers.getAllNotes)
+  .post(notesControllers.createNewNote)
+
+module.exports = router
