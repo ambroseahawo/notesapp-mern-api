b9d05cccb4bc1f2db4cab7109f1e82ab71bd7aac
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-03-27 22:19:47 +0300

    Implement auth controllers logout function

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -91,7 +91,17 @@ const refresh = (req, res) => {
 // @desc Logout
 // @route POST /auth/logout
 // @access Public - just to clear cookies if exists
-const logout = (req, res) => { }
+const logout = (req, res) => {
+  const cookies = req.cookies
+
+  if (!cookies ? jwt) return res.sendStatus(204) // no content
+
+  res.cleanCookie("jwt", {
+    httpOnly: true, sameSite: "None", secure: true
+  })
+
+  res.json({ message: "Cookie cleared" })
+}
 
 module.exports = {
   login,
